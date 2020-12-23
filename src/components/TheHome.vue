edd15ca9c4c6b85e032225c0a209e11dbf8e347d
Edwinfelix2020 <basemon@gmail.com>
2020-12-23 07:57:59 -0500

    first commit

---
+++ src/components/TheHome.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="mt-5">
+
+    <div >
+        <button type="button" class="btn btn-primary" @click="logOut">Logout</button>
+                 
+    </div>
+
+    <div class="container mt-4">
+        <p>Nombre: {{user.name}}</p>
+
+        <p>Correo: {{user.email}}</p>
+
+    </div>    
+
+</div>
+   
+
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            user:{}
+
+        }
+    },
+     methods:{
+        getUserDetails(){
+            let user = localStorage.getItem('user');
+            let token = localStorage.getItem('jwt');
+
+            if(token){
+                this.user = JSON.parse(user);
+            }
+
+        },
+        logOut(){
+            localStorage.removeItem('jwt');
+            localStorage.removeItem('user');
+            this.$router.push('/');
+
+        }
+
+    },
+    created(){
+        this.getUserDetails();
+
+    }
+
+    
+}
+</script>
